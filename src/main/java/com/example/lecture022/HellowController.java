e08e5fc286be78cbaff3fab080ad55e5e2e01fee
Jhon.k <scpo2887@gmail.com>
2025-01-17 11:32:48 +0900

    feat:커밋 테스트
    이 커밋은 테스트 목적입니다
    
    그리움의 미학/ 혹은 과거를 그리워하나

---
+++ src/main/java/com/example/lecture022/HellowController.java
@@ -0,0 +1,14 @@
+package com.example.lecture022;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HellowController {
+	@GetMapping("hello-world")
+	public ResponseEntity<String>HelloWorld(){
+		return ResponseEntity.ok("Hello World");
+	}
+
+}
